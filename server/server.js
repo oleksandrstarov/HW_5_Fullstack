591fa06e760108dd32f66d24f8ae14daf2cfc518
Oleksandr <oleksandr.starov@gmail.com>
2016-04-07 18:10:21 +0000

    v1.0.0 commit

--- server/server.js
+++ server/server.js
@@ -1,30 +1,101 @@
 var express = require('express'),
-    http = require('http');
-    
-var hostname = 'localhost';
-var port = 3000;
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    http = require('http'),
+    fileReader = require('./fileReader.js'),
+    dataBase = require('./dataBase.js');
+
+
+module.exports.serverSettings = {};
+
+module.exports.startServer = function (){
+    app.listen(port, hostname, function(){
+        console.log('server running at http://'+hostname+":"+port+"/"); 
+    });
+};
+
+
+var hostname = this.serverSettings.hostname || process.env.IP || 'localhost';
+var port = this.serverSettings.port || process.env.PORT || 3000;
+
+
 
 var app = express();
+app.use(morgan('dev'));
 
-var htmlData = getHtmlData();
 
-app.use(function(req, res, next){
-    console.log(req.headers);
-    
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/page/html"));
+
+
+app.get('/getFolders', function(req, res){
+    //res.writeHead(200, {'Content-Type':'text/html'});
+    res.end(getRespondData());
+});
+
+/*app.get('/getComents', function(req, res){
+    console.log('get');
     res.writeHead(200, {'Content-Type':'text/html'});
-    res.end(htmlData);
+    res.end(getRespondData());
+});*/
+
+app.post('/saveComment', function(req, res){
+    console.log(req.body);
+    saveDocument(req.body, function(info){
+        res.send(info);
+    });
     
+    /*dataBase.findDocuments(path, function(docs){
+        console.log(docs[0].comment);
+        res.send(docs);
+    });*/
+    
+    //findDocuments();
 });
 
-var server = http.createServer(app);
+app.post('/login', function(req, res){
+    handleLogin(req.body, function(result){
+        res.send(result);
+    });
 
-server.listen(port, hostname, function(){
-   console.log('server running at http://'+hostname+":"+port+"/"); 
+});
+
+app.get('/getComments', function(req, res){
+    findDocuments(function(result){
+        res.send(result);
+    });
+
 });
 
 
 
+function getRespondData(){
+    var data = fileReader.readDirectory();
+    var json = JSON.stringify(data);
+    
+    return json;
+}
 
-function getHtmlData(){
-    return '<html><body><h1>Hello World</h1></body></html>';
+function findDocuments(callback){
+    dataBase.findDocuments(null, function(docs){
+        callback(JSON.stringify(docs));
+    });
+}
+
+function saveDocument(document, callback){
+    dataBase.saveDocumentHandler(document, function(count){
+        callback(count + ' entries being saved');
+    });
+};
+
+function handleLogin(userData, callback){
+    if(userData.eventType === 'signIn'){
+        dataBase.checkLogin(userData, function(response){
+            callback(JSON.stringify(response));
+        });
+    }else{
+        dataBase.registerUser(userData, function(response){
+            callback(JSON.stringify(response));
+        });
+    }
 }
